c72019e9ee1b4fcfc44274d297e23e2f5d9c1972
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-10 12:11:48 +0600

    load data by sport db api

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Players from "../Players/Players";
 import "./Home.css";
 const Home = () => {
+  const [players, setPlayers] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    fetch(
+      `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${search}`
+    )
+      .then((res) => res.json())
+      .then((data) => setPlayers(data?.player));
+  }, [search]);
+  console.log(players);
   return (
     <div>
-      <h2>I am from home</h2>
+      <div className="home-container">
+        <div className="side-container">
+          <input
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+            className="search-input"
+          />
+          <button className="search-btn">Search Players</button>
+          <div className="players-container">
+            <Players></Players>
+          </div>
+        </div>
+        <div className="cart-container">
+          <div className="cart">
+            <p>this is player cart</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/Components/Players/Players.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Players.css";
+const Players = () => {
+  return (
+    <div>
+      <h2>Players Compo</h2>
+    </div>
+  );
+};
+
+export default Players;
